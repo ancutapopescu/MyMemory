e7c0f4e5fc03d54012ca101b150eb1a88bd24813
Ancuta Popescu <s_ancuta@yahoo.com>
2021-11-06 08:10:47 +0200

    Create RecyclerView with Measured Views

---
+++ app/src/main/java/com/example/mymemory/MemoryBoardAdapter.kt
@@ -0,0 +1,47 @@
+package com.example.mymemory
+
+import android.util.Log
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageButton
+import androidx.cardview.widget.CardView
+import androidx.recyclerview.widget.RecyclerView
+import com.example.mymemory.util.Constants.Companion.MARGIN_SIZE
+import kotlin.math.min
+
+class MemoryBoardAdapter(private val context: MainActivity, private val numPieces: Int) :
+    RecyclerView.Adapter<MemoryBoardAdapter.ViewHolder>() {
+
+    companion object {
+        private const val TAG = "MemoryBoardAdapter"
+    }
+
+        inner class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+            private val imageButton = itemView.findViewById<ImageButton>(R.id.imageButton)
+            fun bind(position: Int) {
+                imageButton.setOnClickListener {
+                    Log.i(TAG, "Clicked on position $position")
+                }
+            }
+        }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        val cardWidth = parent.width / 2 - (2 * MARGIN_SIZE)
+        val cardHeight = parent.height / 4 - (2 * MARGIN_SIZE)
+        val cardSideLenght = min(cardWidth, cardHeight)
+        val view = LayoutInflater.from(context).inflate(R.layout.memory_card, parent, false)
+        val layoutParams = view.findViewById<CardView>(R.id.cardView).layoutParams as ViewGroup.MarginLayoutParams
+        layoutParams.width = cardSideLenght
+        layoutParams.height = cardSideLenght
+        layoutParams.setMargins(MARGIN_SIZE, MARGIN_SIZE, MARGIN_SIZE, MARGIN_SIZE)
+        return ViewHolder(view)
+    }
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        holder.bind(position)
+    }
+
+    override fun getItemCount() = numPieces
+
+}
---
+++ app/src/main/java/com/example/mymemory/util/Constants.kt
@@ -0,0 +1,7 @@
+package com.example.mymemory.util
+
+class Constants {
+    companion object {
+        const val MARGIN_SIZE = 16
+    }
+}
